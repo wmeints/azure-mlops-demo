c6bf8f79460ba19e4c2b24a4a089ed08ae824f28
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-07-26 16:51:15 +0200

    Fix scoring script to match training script

--- components/score/score.py
+++ components/score/score.py
@@ -5,9 +5,10 @@ import json
 import pandas as pd
 import logging
 
+model_directory = os.getenv("AZUREML_MODEL_DIR", default="./models")
+model_file = os.path.join(model_directory,
+                          "wachttijden-random-forest/model.pkl")
 
-model_directory = os.getenv("AZUREML_MODEL_DIR")
-model_file = os.path.join(model_directory, "wachttijden-ebm/model.pkl")
 
 def init():
     global model
@@ -20,12 +21,31 @@ def init():
 
     logging.info("Loaded model from %s", model_directory)
 
+
 def run(raw_data):
     data = json.loads(raw_data)["data"]
     df = pd.DataFrame([data])
 
-    logging.info("Features: %s", df.columns)
+    # Note: You'll have to leave the features in this order for the model to work correctly.
+    # If you change the order, the model will mix up the one-hot encoding resulting in the wrong prediction.
+    input_features = [
+        'TYPE_WACHTTIJD', 'SPECIALISME', 'ROAZ_REGIO', 'TYPE_ZORGINSTELLING'
+    ]
 
-    prediction = model.predict(df.to_numpy())
+    prediction = model.predict(df[input_features].to_numpy())
 
     return json.dumps({"prediction": prediction.tolist()})
+
+
+if __name__ == "__main__":
+    sample_data = json.dumps({
+        "data": {
+            "TYPE_WACHTTIJD": "Behandeling",
+            "SPECIALISME": "KNO (302)",
+            "ROAZ_REGIO": "Noord-NL",
+            "TYPE_ZORGINSTELLING": "Ziekenhuis"
+        }
+    })
+
+    init()
+    print(run(sample_data))
