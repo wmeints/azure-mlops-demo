f5a16b4e006379d3db4c118b5e5dca9ac1df72ea
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-09-09 09:21:33 +0200

    Fix path to component YAML files

--- create-components.ps1
+++ create-components.ps1
@@ -2,8 +2,8 @@ $ResourceGroup = $env:AZURE_ML_RESOURCE_GROUP
 $WorkspaceName = $env:AZURE_ML_WORKSPACE
 
 $components = @(
-    "./components/fix_missing_values"
-    "./components/select_features"
+    "./components/fix_missing_values/fix_missing_values.yml"
+    "./components/select_features/select_features.yml"
 )
 
 foreach($component in $components) {
