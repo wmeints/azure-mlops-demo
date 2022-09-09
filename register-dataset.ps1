222a807cfc8c97b31fc8f44fb85d90dc1441dc2f
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-09-09 09:22:31 +0200

    Fix commandline options for dataset registration command

--- register-dataset.ps1
+++ register-dataset.ps1
@@ -1,4 +1,4 @@
 $ResourceGroup = $env:AZURE_ML_RESOURCE_GROUP
 $WorkspaceName = $env:AZURE_ML_WORKSPACE
 
-az ml dataset create --file ./data/wachttijden_raw.yml --resource-group $ResourceGroup --workspace $WorkspaceName
+az ml data create --file ./data/wachttijden_raw.yml --resource-group $ResourceGroup --workspace $WorkspaceName
